024fc50966a48e9710210600ebe5a6712fd9c9b3
blushingchutney <blushingchutney@users.noreply.github.com>
2024-11-07 23:06:13 -0500

    Adding sound and delay

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <dos.h>
+#include <pc.h>
 
 #include <allegro.h>
 
@@ -7,32 +8,57 @@
 #include <lauxlib.h>
 #include <luaconf.h>
 
-int allegro_lua_readkey(lua_State *L) {
+int MyGameEngine_lua_readkey(lua_State *L) {
     int k = readkey();
 
     lua_pushnumber(L, k);
     return 1;
 }
 
+int MyGameEngine_lua_sound(lua_State *L) {
+    int f = lua_tonumber(L, 1);
+    sound(f);
+    return 0;
+}
+
+int MyGameEngine_lua_delay(lua_State *L) {
+    int f = lua_tonumber(L, 1);
+    delay(f);
+    return 0;
+}
+
 int main(int argc, char const *argv[])
 {
+    // Init allegro
     allegro_init();
     install_keyboard();
     set_gfx_mode(GFX_AUTODETECT, 320, 200, 0, 0);
 
+    // Init lua
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
 
+    // Adding MyGameEngine table
     lua_newtable(L);
 
     lua_pushstring(L, "readkey");
-    lua_pushcfunction(L, allegro_lua_readkey);
+    lua_pushcfunction(L, MyGameEngine_lua_readkey);
     lua_settable(L, -3);
 
-    lua_setglobal(L, "allegro");
+    lua_pushstring(L, "sound");
+    lua_pushcfunction(L, MyGameEngine_lua_sound);
+    lua_settable(L, -3);
 
+    lua_pushstring(L, "delay");
+    lua_pushcfunction(L, MyGameEngine_lua_delay);
+    lua_settable(L, -3);
+
+    lua_setglobal(L, "MyGameEngine");
+
+    // Do game code
     luaL_dofile(L, "main.lua");
 
+    // Shutdown lua
     lua_close(L);
 
     return 0;
--- main.lua
+++ main.lua
@@ -1,2 +1,21 @@
 print("Hello, World!!!")
-allegro.readkey()
+
+chirp_frequencies = {
+    329.63, 329.63, 349.23, 392.00, -- E E F G
+    392.00, 349.23, 329.63, 293.66, -- G F E D
+    261.63, 261.63, 293.66, 329.63, -- C C D E
+    329.63, 293.66, 293.66,          -- E D D (pause)
+    
+    329.63, 329.63, 349.23, 392.00, -- E E F G
+    392.00, 349.23, 329.63, 293.66, -- G F E D
+    261.63, 261.63, 293.66, 329.63, -- C C D E
+    293.66, 261.63, 261.63           -- D C C
+}
+for i = 1, #chirp_frequencies do
+    MyGameEngine.sound(chirp_frequencies[i])
+    MyGameEngine.delay(100)
+end
+MyGameEngine.sound(0)
+
+print("Press any key to exit")
+MyGameEngine.readkey()
